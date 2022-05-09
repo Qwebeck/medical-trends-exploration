fdd327bb474c1fd942e31136e1e52851e22ebfbe
justyna3773 <justyna3773@gmail.com>
2022-05-09 09:49:33 +0200

    added plots for number of 19-year-olds in Polnad, age pyramid and emmigration

--- src/data/make_bdl.py
+++ src/data/make_bdl.py
@@ -31,22 +31,26 @@ def make_bdl(save_to_folder: Path, should_overwrite: bool):
         for var_name in variable_set:
             logging.info(f'obtaining {var_name}')
             year_range = range(2010, 2022)
-            file_path = save_to_folder / 'medical_staff_stats_bdl' / variable_set_name / f'{var_name}_summary.csv'
+            file_path = save_to_folder / 'medical_staff_stats_bdl' / \
+                variable_set_name / f'{var_name}_summary.csv'
             if not file_path.parent.exists():
                 file_path.parent.mkdir(parents=True)
             if file_path.exists() and not should_overwrite:
                 logging.info(f"Skipping {var_name} because it already exists")
                 continue
-            data = _collect_data_over_years(variable_set[var_name], var_name, year_range)
+            data = _collect_data_over_years(
+                variable_set[var_name], var_name, year_range)
             data.to_csv(file_path)
 
 
 def _collect_data_over_years(var_id, var_name, year_range) -> pd.DataFrame:
     variable_values_over_years = pd.DataFrame(columns=[var_name])
     for year in year_range:
-        data = get_complete_gus_data(Level.COUNTRY, [var_id], year, {var_name: var_id})
+        data = get_complete_gus_data(
+            Level.COUNTRY, [var_id], year, {var_name: var_id})
         if data is not None and var_name in data.columns:
-            variable_values_over_years.loc[year, var_name] = data.loc[0, var_name]
+            variable_values_over_years.loc[year,
+                                           var_name] = data.loc[0, var_name]
         else:
             logging.info(f"No info provided for year {year}")
     return variable_values_over_years
